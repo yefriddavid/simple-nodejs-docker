46b833d2504c985bf1cbf8a5a11aa5c30d95b2ae
Jeffrey David Rios <yefriddavid@gmail.com>
2021-06-26 11:23:43 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World'  + process.env.NODE_ENV);
 });
 
 app.listen(PORT, HOST);
